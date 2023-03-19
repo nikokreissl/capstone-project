7602e6f2f8db66b5fb0986ba0042b468c35cc5fc
Niko Kreissl <nikokreissl@gmail.com>
2023-03-19 14:54:15 +0100

    Add competition sample data

---
+++ data/competition.js
@@ -0,0 +1,14 @@
+export const competition = [
+  {
+    name: "Weekend League",
+    totalGames: 15,
+    isArchived: false,
+    gamesPlayed: [],
+  },
+  {
+    name: "Weekend League Qualification",
+    totalGames: 10,
+    isArchived: false,
+    gamesPlayed: [],
+  },
+];
