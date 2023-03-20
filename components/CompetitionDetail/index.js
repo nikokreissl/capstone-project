2e40688ad4f853b277ef1beb287f62aca7d9af78
Niko Kreissl <nikokreissl@gmail.com>
2023-03-20 10:48:52 +0100

    Create CompetitionDetail Component

---
+++ components/CompetitionDetail/index.js
@@ -0,0 +1,18 @@
+export default function CompetitionDetail({ competition }) {
+  return (
+    <main>
+      <button>🔙 Back</button>
+      <button>⚙️ Edit</button>
+      <h2>{competition.name}</h2>
+      <h3>Details</h3>
+      <p>Wins: X / Loses: X</p>
+      <button>Track Game</button>
+      <ul>
+        <li>Game 1</li>
+        <li>Game 2</li>
+        <li>Game 3</li>
+        <li>Game 4</li>
+      </ul>
+    </main>
+  );
+}
--- pages/competition/[competitionId].js
+++ pages/competition/[competitionId].js
@@ -1,7 +1,16 @@
 import { useRouter } from "next/router";
+import CompetitionDetail from "../../components/CompetitionDetail";
+import { competitions } from "../../data/competition";
 
 export default function CompetitionDetailPage() {
   const router = useRouter();
   const { competitionId } = router.query;
-  return <div>{competitionId}</div>;
+
+  const competition = competitions.find(
+    (competition) => competition.id === competitionId
+  );
+
+  console.log(competition);
+
+  return <CompetitionDetail competition={competition} />;
 }
