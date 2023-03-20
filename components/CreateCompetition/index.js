85002649170688fcc135c3bfd125e650501a8698
Niko Kreissl <nikokreissl@gmail.com>
2023-03-20 16:05:08 +0100

    Add very basic styles and rules to form

--- components/CreateCompetition/index.js
+++ components/CreateCompetition/index.js
@@ -1,14 +1,41 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 export default function CreateCompetitionForm() {
+  const [competitionNameInput, setCompetitionNameInput] = useState("");
+  const [competitionGameInput, setCompetitionGameInput] = useState(1);
+
+  function handleNameInput(event) {
+    setCompetitionNameInput(event.target.value);
+  }
+
+  function handleGameInput(event) {
+    setCompetitionGameInput(event.target.value);
+  }
+
   return (
     <>
       <button>Cancel</button>
       <StyledCreateCompetitionForm>
         <label htmlFor="competition-name">Name</label>
-        <input type="text" name="competition-name" id="competition-name" />
+        <input
+          type="text"
+          name="competition-name"
+          id="competition-name"
+          value={competitionNameInput}
+          onChange={handleNameInput}
+          required
+        />
         <label htmlFor="competition-games">Number of Games</label>
-        <input type="number" name="competition-games" id="competition-games" />
+        <input
+          type="number"
+          name="competition-games"
+          id="competition-games"
+          value={competitionGameInput}
+          onChange={handleGameInput}
+          min={1}
+          max={100}
+        />
         <button>Create competition</button>
       </StyledCreateCompetitionForm>
     </>
