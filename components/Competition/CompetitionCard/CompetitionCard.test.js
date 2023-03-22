b3898e9492dbded772eca6529ac0198c5fc620f3
Niko Kreissl <nikokreissl@gmail.com>
2023-03-22 14:25:20 +0100

    Add CompetitionCard test

---
+++ components/Competition/CompetitionCard/CompetitionCard.test.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import CompetitionCard from ".";
+
+test("Rendes information on Card correcty", () => {
+  const competition = {
+    id: 1,
+    name: "Competition Name",
+    totalGames: 12,
+    gamesPlayed: [
+      {
+        userScore: 1,
+        opponentScore: 0,
+      },
+      {
+        userScore: 2,
+        opponentScore: 0,
+      },
+      {
+        userScore: 0,
+        opponentScore: 1,
+      },
+    ],
+  };
+  render(<CompetitionCard competition={competition} />);
+
+  const competitionName = screen.getByRole("heading", {
+    name: "Competition Name",
+  });
+
+  const record = screen.getByText("Record: Wins 2 / Loses 1");
+
+  const remainingGames = screen.getByText("Remaining Games: 9");
+
+  expect(record).toBeInTheDocument();
+  expect(competitionName).toBeInTheDocument();
+  expect(remainingGames).toBeInTheDocument();
+});
