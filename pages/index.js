9ab782c4333e559c17950c78186a941be7ea00e4
Niko Kreissl <nikokreissl@gmail.com>
2023-03-24 11:21:13 +0100

    Add ObjectiveCard test

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,66 @@
 import { useContext } from "react";
 import { DataContext } from "./_app";
-import { StyledDetailsLink } from "../components/Competition/CompetitionCard/StyledCompetitionCard";
 import CompetitionList from "../components/Competition/CompetitionList";
 import ObjectiveList from "../components/Objectives/ObjectiveList";
-import { StyledButtonWrapper } from "../components/GeneralComponents/Buttons/StyledButton";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styled from "styled-components";
 
 export default function Home() {
+  const router = useRouter();
   const { competitions, objectives } = useContext(DataContext);
+
+  const [selectedCreateItem, setSelectedCreateItem] = useState();
 
   if (!competitions || !objectives) {
     return <div>Loading...</div>;
   }
 
+  function handleSelectItemChange(event) {
+    setSelectedCreateItem(event.target.value);
+  }
+
+  function handleCreateItem(event) {
+    event.preventDefault();
+    console.log(selectedCreateItem);
+    if (selectedCreateItem) {
+      router.push(`${selectedCreateItem}/create`);
+    } else if (!selectedCreateItem) {
+      alert("Please select what you want to create");
+    }
+  }
+
   return (
     <>
       <main>
-        <StyledButtonWrapper>
-          <StyledDetailsLink href={"/competition/create"}>
-            Create new competition
-          </StyledDetailsLink>
-          <StyledDetailsLink href={"/objective/create"}>
-            Create new objective
-          </StyledDetailsLink>
-        </StyledButtonWrapper>
+        <StyledCreateItemForm onSubmit={handleCreateItem}>
+          <StyledCreateItemSelect
+            value={selectedCreateItem}
+            onChange={handleSelectItemChange}
+            name="create-element"
+            id="create-element"
+          >
+            <option value="">Select item...</option>
+            <option value="competition">Competition</option>
+            <option value="objective">Objective</option>
+          </StyledCreateItemSelect>
+          <button>Create</button>
+        </StyledCreateItemForm>
         <CompetitionList competitions={competitions} headline="Competitions" />
         <ObjectiveList objectives={objectives} headline="Objectives" />
       </main>
     </>
   );
 }
+
+const StyledCreateItemForm = styled.form`
+  width: 80vw;
+  margin: 20px;
+  display: flex;
+  justify-content: center;
+  gap: 0.5em;
+`;
+
+const StyledCreateItemSelect = styled.select`
+  width: 30vw;
+`;
