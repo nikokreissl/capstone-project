1215ab906f9d20e2016ff47a2e769c38c97d6684
Niko Kreissl <nikokreissl@gmail.com>
2023-03-27 17:09:21 +0200

    Add archive card

--- pages/archive/index.js
+++ pages/archive/index.js
@@ -1,5 +1,21 @@
-import React from "react";
+import ArchiveCard from "../../components/Archive/ArchiveCards";
+import { useContext } from "react";
+import { DataContext } from "../_app";
 
-export default function index() {
-  return <div>index</div>;
+export default function ArchivePage() {
+  const { objectives, competitions } = useContext(DataContext);
+  return (
+    <main>
+      <ArchiveCard
+        itemName="Competitions"
+        object={competitions}
+        href="competitions"
+      />
+      <ArchiveCard
+        itemName="Objectives"
+        object={objectives}
+        href="competitions"
+      />
+    </main>
+  );
 }
