e7602483329d47089c8aa9039786aee16b506d26
Niko Kreissl <nikokreissl@gmail.com>
2023-04-13 09:39:18 +0200

    Update archive card view

--- components/Archive/ArchiveCards/index.js
+++ components/Archive/ArchiveCards/index.js
@@ -1,20 +1,33 @@
+import styled from "styled-components";
 import { StyledLinkComponent } from "../../GeneralComponents/Links";
-import {
-  StyledContainer,
-  StyledHeadline,
-  StyledParagraph,
-} from "../../GeneralComponents/Cards";
+import { StyledContainer, StyledHeadline } from "../../GeneralComponents/Cards";
 
 export default function ArchiveCard({ itemName, object, href }) {
   const archivedItems = object.filter((item) => item.isArchived === true);
 
   return (
     <StyledContainer>
-      <StyledHeadline>All {itemName}</StyledHeadline>
-      <StyledParagraph>Total: {archivedItems.length}</StyledParagraph>
+      <StyledHeadline>{itemName}</StyledHeadline>
+      <StyledArchiveCardContainer>
+        <h4>Total number of {itemName}</h4>
+        <StyledArchiveCardText>{archivedItems.length}</StyledArchiveCardText>
+      </StyledArchiveCardContainer>
       <StyledLinkComponent type="view" href={href}>
         View Details
       </StyledLinkComponent>
     </StyledContainer>
   );
 }
+
+const StyledArchiveCardContainer = styled.div`
+  margin: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const StyledArchiveCardText = styled.p`
+  font-weight: bold;
+  color: var(--orange);
+  font-size: 1.2rem;
+`;
